4bd74d3e8923909479ad206b55818d45b6874490
Steve Hull <hullstevenj@gmail.com>
2025-03-18 15:37:59 -0700

    Added routes for goals, user, and 404 page not found in app.js

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const authRoutes = require('./routes/auth');
+const goalsRoutes = require('./routes/goals');
+const userRoutes = require('./routes/user');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(authRoutes);
+app.use(goalsRoutes);
+app.use(userRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000)
 
