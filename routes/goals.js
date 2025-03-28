af08f52d7a0d8227279852167596a11d84553de1
Steve Hull <hullstevenj@gmail.com>
2025-03-28 14:36:33 -0700

    Updated goals.js to play nicely with Swagger and Postman

--- routes/goals.js
+++ routes/goals.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/goals', (req, res, next) => {
-    res.send('<h1>This is the goals page</h1>');
+router.get('/', (req, res, next) => {
+    res.status(200).json({ message: 'This is the goals page' });
 });
 
-router.post('/goals', (req, res, next) => {
-    res.send('<h1>This is the create a new goal page</h1>');
+router.post('/', (req, res, next) => {
+    res.status(200).json({ message: 'This is the create a new goal page' });
 });
 
 module.exports = router;
