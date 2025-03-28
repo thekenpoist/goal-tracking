f7a94be360792a540ec40cff60dbd16a95bccd42
Steve Hull <hullstevenj@gmail.com>
2025-03-28 14:38:56 -0700

    Updated auth.js to play nicely with Swagger and Postman

--- routes/auth.js
+++ routes/auth.js
@@ -3,19 +3,19 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
-    res.send('<h1>This is the sign up page</h1>');
+    res.status(200).json({ message: 'This is the sign up page</h1>' });
 });
 
 router.post('/login', (req, res, next) => {
-    res.send('<h1>Login page</h1>');
+    res.status(200).json({ message: 'Login page' });
 });
 
 router.post('/logout', (req, res, next) => {
-    res.send('<h1>Log out page</h1>');
+    res.status(200).json({ message: 'Log out page' });
 });
 
 router.post('/forgot-password', (req, res, next) => {
-    res.send('<h1>Forgot password page</h1>');
+    res.status(200).json({ message: 'Forgot password page' });
 });
 
 module.exports = router;
