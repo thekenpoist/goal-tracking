ba698d6a0697e44c29becf3c790bf30be31ee3bf
Steve Hull <hullstevenj@gmail.com>
2025-03-29 15:32:24 -0700

    Modified app.js to enable the view engine for Pug templating

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerui = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const path = require('path');
 
 const app = express();
 
@@ -9,6 +10,9 @@ const authRouter = require('./routes/auth');
 const goalsRouter = require('./routes/goals');
 const userRouter = require('./routes/users');
 const indexRouter = require('./routes/index');
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use('/auth', authRouter);
 app.use('/goals', goalsRouter);
