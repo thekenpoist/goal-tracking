e50f8f391a8e073b673894d1fc65099f5ffecaef
Steve Hull <hullstevenj@gmail.com>
2025-03-30 09:44:29 -0700

    Created goals.pug and changes to goals.js and header.pug for goals link

--- routes/goals.js
+++ routes/goals.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({ message: 'This is the goals page' });
+    res.render('goals', { title: 'goals', currentPage: 'goals'});
 });
 
 router.post('/', (req, res, next) => {
