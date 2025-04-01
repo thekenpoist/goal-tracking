34653c5a1fa44698361c976e2f8128da782c8c5d
Steve Hull <hullstevenj@gmail.com>
2025-03-31 20:05:37 -0700

    Removed require('body-parser') and implemented express.urlencoded for form handling instead in app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const swaggerui = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const path = require('path');
@@ -20,8 +19,7 @@ app.use('/users', userRouter);
 app.use('/', indexRouter);
 
 app.use('/api-docs', swaggerui.serve, swaggerui.setup(swaggerDocument));
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Page not found' });
