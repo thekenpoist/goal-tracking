39acb2e5b0bd9ded27c166a7811f5e5173367041
Steve Hull <hullstevenj@gmail.com>
2025-04-07 12:31:42 -0700

    Create layout.ejs for styling

--- app.js
+++ app.js
@@ -8,15 +8,20 @@ const app = express();
 //const authRouter = require('./routes/auth');
 //const goalsRouter = require('./routes/goals');
 //const userRouter = require('./routes/users');
-const homeRouter = require('./routes/index');
+const homeRouter = require('./routes/homeRoutes');
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use('/auth', authRouter);
-app.use('/goals', goalsRouter);
-app.use('/users', userRouter);
-app.use('/', indexRouter);
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+//app.use('/auth', authRouter);
+//app.use('/goals', goalsRouter);
+//app.use('/users', userRouter);
+app.use('/', homeRouter);
 
 app.use('/api-docs', swaggerui.serve, swaggerui.setup(swaggerDocument));
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +29,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Page not found' });
 });
+
+
 
 
 
