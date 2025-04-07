dcf6b08ed46f89a72a692bbad5beb340c59972a9
Steve Hull <hullstevenj@gmail.com>
2025-04-07 12:48:41 -0700

    Refactor app.js for organization for best industry standard

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const path = require('path');
 const swaggerui = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 
-const app = express();
 
 //const authRouter = require('./routes/auth');
 //const goalsRouter = require('./routes/goals');
 //const userRouter = require('./routes/users');
 const homeRouter = require('./routes/homeRoutes');
 
+const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(expressLayouts);
+app.set('layout', 'layout');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.locals.path = req.path;
@@ -24,14 +33,11 @@ app.use((req, res, next) => {
 app.use('/', homeRouter);
 
 app.use('/api-docs', swaggerui.serve, swaggerui.setup(swaggerDocument));
-app.use(express.urlencoded({ extended: false }));
+
 
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Page not found' });
 });
-
-
-
 
 
 app.listen(3000);
