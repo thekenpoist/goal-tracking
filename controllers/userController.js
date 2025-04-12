b72963babda30086f1c1a2350c6793188a0ba5ed
Steve Hull <hullstevenj@gmail.com>
2025-04-12 07:33:43 -0700

    Add postAddUser function to userController.js to write user profile to users.json

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,6 +16,13 @@ exports.getAddUser = (req, res, next) => {
 };
 
 exports.postAddUser = (req, res, next) => {
-    console.log(req.body);
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const realName = req.body.realName;
+    const avatar = req.body.avatar;
+
+    const user = new User(null, username, email, password, realName, avatar);
+    user.saveUsers();
     res.redirect('/');
 };
