a7d48ba988a5666e37dd54f4a1a2fe7cd5ab4b9f
Steve Hull <hullstevenj@gmail.com>
2025-04-17 12:50:03 -0700

    Refactor dashboard-header.ejs and userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,5 @@
 const User = require("../models/userModel");
 
-exports.getUserById = (req, res, next) => {
-    const userId = req.params.id;
-    User.getUserByID(userId, user => {
-        if (!user) return res.status(404).json({ error: 'User not found' });
-        res.render('users/profile', { user });
-    });
-};
-
 exports.getAddUser = (req, res, next) => {
     res.render('profiles/new-profile', {
         pageTitle: "Create Profile",
