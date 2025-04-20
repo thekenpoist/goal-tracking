d0ddc00d7d9a15a04009ccd3dfad5268c30571ff
Steve Hull <hullstevenj@gmail.com>
2025-04-20 13:04:32 -0700

    Create getGoalsFromFile function in goalModel.js

---
+++ models/goalModel.js
@@ -0,0 +1,15 @@
+const fsPromises = require('fs').promises;
+const path = require('path');
+const { error } = require('console');
+
+const p = path.join(__dirname, '..', 'data', 'goals.json');
+
+const getGoalsFromFile = async () => {
+    try {
+        const fileContent = await fsPromises.readFile(p, 'utf-8');
+        return JSON.parse(fileContent);
+    } catch (err) {
+        console.warn('Error reading goals.json. Returning empty array.');
+        return [];
+    }
+};
