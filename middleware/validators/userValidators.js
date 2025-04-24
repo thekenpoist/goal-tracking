c2c7e8ee8afaa65ca8356da7721cd607be7ddd77
Steve Hull <hullstevenj@gmail.com>
2025-04-24 15:53:45 -0700

    Dry refactor userRoutes.js and userValidators.js for addUser form validation

---
+++ middleware/validators/userValidators.js
@@ -0,0 +1,19 @@
+const { body } = require('express-validator')
+
+exports.addUserRules = [
+    body('username')
+        .isLength({ min: 4, max: 50 }).withMessage('Username must be between 4 and 50 characters.'),
+    body('email')
+        .isEmail().withMessage('A valid email is required.')
+        .normalizeEmail(),
+    body('password')
+        .isLength({ min: 10, max: 25 }).withMessage('Password must be between 10 and 25 characters.'),
+    body('realName')
+        .optional({ checkFalsy: true })
+        .isLength({ max: 50 }).withMessage('Real name must be less than 50 characters.'),
+    body('avatar')
+        .optional({ checkFalsy: true })
+        .isURL().withMessage('Avatar must be a valid URL.')
+        .isLength({ max: 2048 }).withMessage('Avatar URL is too long.')
+
+];
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const { body } = require('express-validator');
+const { addUserRules } = require('../middleware/validators/userValidators');
 
 const userController = require('../controllers/userController');
 
@@ -8,25 +9,7 @@ const router = express.Router();
 
 router.get('/new-profile', userController.getAddUser);
 
-
-router.post('/', [
-    body('username')
-        .isLength({ min: 4, max: 50 }).withMessage('Username must be between 4 and 50 characters.'),
-    body('email')
-        .isEmail().withMessage('A valid email is required.')
-        .normalizeEmail(),
-    body('password')
-        .isLength({ min: 10, max: 25 }).withMessage('Password must be between 10 and 25 characters.'),
-    body('realName')
-        .optional({ checkFalsy: true })
-        .isLength({ max: 50 }).withMessage('Real name must be less than 50 characters.'),
-    body('avatar')
-        .optional({ checkFalsy: true })
-        .isURL().withMessage('Avatar must be a valid URL.')
-        .isLength({ max: 2048 }).withMessage('Avatar URL is too long.')
-
-], userController.postAddUser);
-
+router.post('/', addUserRules, userController.postAddUser);
 
 router.get('/edit-profile/:userId', userController.getEditUser);
 
