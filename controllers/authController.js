71ee60b52d15c145a764a40f2315e9ca85ff348b
Steve Hull <hullstevenj@gmail.com>
2025-04-26 12:35:33 -0700

    Refactor getLogin and postLogin from userController.js to authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,48 @@
+const { validationResult } = require("express-validator");
+const User = require("../models/userModel");
+
+exports.getLogin = async (req, res, next) => {
+    res.render('auth/login', {
+        pageTitle: 'Login',
+        currentPage: 'login',
+        errorMessage: null
+    });
+};
+
+exports.postLogin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.getUserByEmail(email);
+
+        if (!user) {
+            return res.status(401).render('auth/login', {
+                pageTitle: 'Login',
+                currentPage: 'login',
+                errorMessage: 'Invalid email or password.'
+            });
+        }
+        if (user.passwordHash !== password) {
+            return res.status(401).render('auth/login', {
+                pageTitle: 'Login',
+                currentPage: 'login',
+                errorMessage: 'Invalid email or password'
+            });
+        }
+
+        req.session.userId = user.uuid;
+        req.session.save(err => {
+            if (err) {
+                console.error('Session save error', err);
+            }
+            res.redirect('/');
+        });
+    } catch (err) {
+        console.error('Login error', err);
+        res.status(500).render('auth/login', {
+            pageTitle: 'Login',
+            currentPage: 'login',
+            errorMessage: 'An error occurred. Please try again.'
+        });
+    }
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -46,52 +46,6 @@ exports.postAddUser = async (req, res, next) => {
 
 };
 
-exports.getLogin = async (req, res, next) => {
-    res.render('auth/login', {
-        pageTitle: 'Login',
-        currentPage: 'login',
-        errorMessage: null
-    });
-};
-
-exports.postLogin = async (req, res, next) => {
-    const { email, password } = req.body;
-
-    try {
-        const user = await User.getUserByEmail(email);
-
-        if (!user) {
-            return res.status(401).render('auth/login', {
-                pageTitle: 'Login',
-                currentPage: 'login',
-                errorMessage: 'Invalid email or password.'
-            });
-        }
-        if (user.passwordHash !== password) {
-            return res.status(401).render('auth/login', {
-                pageTitle: 'Login',
-                currentPage: 'login',
-                errorMessage: 'Invalid email or password'
-            });
-        }
-
-        req.session.userId = user.uuid;
-        req.session.save(err => {
-            if (err) {
-                console.error('Session save error', err);
-            }
-            res.redirect('/');
-        });
-    } catch (err) {
-        console.error('Login error', err);
-        res.status(500).render('auth/login', {
-            pageTitle: 'Login',
-            currentPage: 'login',
-            errorMessage: 'An error occurred. Please try again.'
-        });
-    }
-};
-
 exports.getEditUser = async (req, res, next) => {
     const userId = req.params.userId;
 
