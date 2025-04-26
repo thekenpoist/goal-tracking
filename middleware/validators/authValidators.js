4aa728b11fb9b919a8c21aa5ac041c6abd754572
Steve Hull <hullstevenj@gmail.com>
2025-04-26 14:40:21 -0700

    Add signupRules to authValidators.js

--- middleware/validators/authValidators.js
+++ middleware/validators/authValidators.js
@@ -7,3 +7,27 @@ exports.loginRules = [
     body('password')
         .isLength({ min: 10, max: 25 }).withMessage('Password must be between 10 and 25 characters.'),
 ];
+
+const { body } = require('express-validator')
+
+exports.signupRules = [
+    body('email')
+        .isEmail().withMessage('A valid email is required.')
+        .normalizeEmail(),
+    body('confirmEmail')
+        .custom((value, { req }) => {
+            if (value !== req.body.email) {
+                throw new Error('Emails do not match.');
+            }
+            return true;
+        }),
+    body('password')
+        .isLength({ min: 10, max: 25 }).withMessage('Password must be between 10 and 25 characters.'),
+    body('confirmPassword')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords do not match.');
+            }
+            return true;
+        }),
+];
