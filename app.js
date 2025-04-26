1be5e3bc02fa215e0d952cadc48ccdf9309f60dc
Steve Hull <hullstevenj@gmail.com>
2025-04-26 10:15:23 -0700

    Refactor app.js for session authentication with express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const swaggerui = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const expressLayouts = require('express-ejs-layouts');
+const sesson = require('express-session');
 
 const errorController = require('./controllers/errorController');
 const homeRouter = require('./routes/homeRoutes');
@@ -18,6 +19,13 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(expressLayouts);
 app.set('layout', 'layouts/main-layout');
+
+app.use(session({
+    secret: '4d3bca5fa3e385395f69bac991078cef4eef4be66f1f816503bdc0a8359639f14a9618efa3efbd3646e5ebab9343c327516c48e195ef8397adaf70077a4de836',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
