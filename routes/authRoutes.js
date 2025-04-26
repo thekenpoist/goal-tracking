ddc99acf2abcd9e7f4a1442b2161d24f15600c3c
Steve Hull <hullstevenj@gmail.com>
2025-04-26 12:42:48 -0700

    Refactor authRoutes.js to use authController

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,12 +2,12 @@ const path = require('path');
 const express = require('express');
 const { body } = require('express-validator');
 
-const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get('/login', userController.getLogin);
-router.post('/login', userController.postLogin);
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
 
 
 
