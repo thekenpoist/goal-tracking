aea17c4cb65d7d8f4f15eca70263213eef85786f
Steve Hull <hullstevenj@gmail.com>
2025-04-26 14:41:31 -0700

    Refactor authRoutes.js with signupRules

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/signup', authController.getSignup);
-router.post('/signup', addUserRules, authController.postSignup);
+router.post('/signup', signupRules, authController.postSignup);
 
 router.get('/login', authController.getLogin);
 router.post('/login', loginRules, authController.postLogin);
