322c58ab5f36aeb025dfe53c00ee4f1396fabce1
Steve Hull <hullstevenj@gmail.com>
2025-04-26 13:30:03 -0700

    Refactor postLogin route in authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const express = require('express');
 const { body } = require('express-validator');
+const { loginRules } = require('..middleware/validators/authValidators');
 
 const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
-router.post('/login', authController.postLogin);
+router.post('/login', loginRules, authController.postLogin);
 
 
 
