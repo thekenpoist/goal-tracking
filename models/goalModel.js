48dcbb59fbbd712fc1164ef7974edaf071d3cbc3
Steve Hull <hullstevenj@gmail.com>
2025-05-15 15:47:13 -0700

    Create Goal.init in goalModel and finish goalModel refactor

--- models/goalModel.js
+++ models/goalModel.js
@@ -11,4 +11,58 @@ module.exports = (sequelize) => {
             });
         }
     }
+
+    Goal.init({
+        uuid: {
+            type:DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        userUuid: {
+            type:DataTypes.UUID,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING
+        },
+        description: {
+            type: DataTypes.TEXT
+        },
+        priority: {
+            type: DataTypes.STRING
+        },
+        startDate: {
+            type: DataTypes.DATE
+        },
+        endDate: {
+            type: DataTypes.DATE
+        },
+        frequency: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        duration: {
+            type: DataTypes.INTEGER, // Hey Steve, be thinking in minutes, but not yet certain
+            allowNull: true
+        },
+        isCompleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        wasAchieved: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    }, {
+        sequelize,
+        modelName: 'Goal',
+        tableName: 'goals',
+        timestamps: true
+    });
+    
+    return Goal;
 };
