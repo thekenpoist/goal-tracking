6d1712b077cc5c172943797d041d1054517789bb
Steve Hull <hullstevenj@gmail.com>
2025-05-18 09:58:51 -0700

    work on getCalendar in goalLogController

--- controllers/goalLogController.js
+++ controllers/goalLogController.js
@@ -17,10 +17,13 @@ exports.getCalendar = async (res, req, next) => {
                 goalUuid
             }
         });
-    }
 
-    if (!goalLog) {
-        return res.status(404).send('<p class="text-red-500">No goals logged.</p>');
+        if (!goalLog) {
+            return res.status(404).send('<p class="text-red-500">No calendar to display..</p>');
+        }
+    } catch (err) {
+        console.error('Error loading calendar.', err);
+        res.status(500).send('<p> classe="text-red-500">Server error loading calendar</p>');
     }
 
 };
