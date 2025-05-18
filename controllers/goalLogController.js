28c65c984d2166d1f4a5d645ad6eb39a2127c931
Steve Hull <hullstevenj@gmail.com>
2025-05-18 09:42:39 -0700

    Code postGoalLog in goalLogController

--- controllers/goalLogController.js
+++ controllers/goalLogController.js
@@ -22,5 +22,16 @@ exports.getCalendar = async (res, req, next) => {
     if (!goalLog) {
         return res.status(404).send('<p class="text-red-500">No goals logged.</p>');
     }
-    
+
+};
+
+exports.postGoalLog = async (res, req, next) => {
+    const userUuid = req.session.userUuid;
+    const goalUuid = req.params.goalUuid;
+    const goalLogUuid = req.params.goalLogUuid;
+
+    if (!userUuid) {
+        return res.render('/auth/login');
+    }
+
 };
