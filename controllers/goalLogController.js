119950603a6666a815bca6b78f30b2faaef188d3
Steve Hull <hullstevenj@gmail.com>
2025-05-18 09:38:43 -0700

    Code getCalendar in goalLogController

---
+++ controllers/goalLogController.js
@@ -0,0 +1,26 @@
+const { GoalLog } = require('../models');
+const { Goal } = require('../models');
+const { DATE } = require('sequelize');
+const { renderServerError } = require('../utils/errorHelpers');
+
+exports.getCalendar = async (res, req, next) => {
+    const userUuid = req.session.userUuid;
+    const goalUuid = req.params.goalUuid;
+
+    if (!userUuid) {
+        return res.render('/auth/login');
+    }
+
+    try {
+        const goalLog = await GoalLog.findall({
+            where: {
+                goalUuid
+            }
+        });
+    }
+
+    if (!goalLog) {
+        return res.status(404).send('<p class="text-red-500">No goals logged.</p>');
+    }
+    
+};
