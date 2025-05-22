905afba362b31f02093c0add8b4bb877e22102ce
Steve Hull <hullstevenj@gmail.com>
2025-05-22 09:16:00 -0700

    Refactor config/config.js to quiet logging

--- config/config.js
+++ config/config.js
@@ -6,20 +6,23 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT
+        dialect: process.env.DB_DIALECT,
+        logging: process.env.DB_LOGGING
     },
     test: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT
+        dialect: process.env.DB_DIALECT,
+        logging: process.env.DB_LOGGING
     },
     production: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT
+        dialect: process.env.DB_DIALECT,
+        logging: process.env.DB_LOGGING
     },
 };
