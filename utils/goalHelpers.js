aeca0d3e3ed51f42e849cafb48c071d522624e2a
Steve Hull <hullstevenj@gmail.com>
2025-05-26 13:31:03 -0700

    Refactor getCurrentCalendarWeek in goalHelpers for timezone offset

--- utils/goalHelpers.js
+++ utils/goalHelpers.js
@@ -1,23 +1,25 @@
 const { utcToZonedTime, zonedTimeToUtc } = require('date-fns-tz');
 const { startOfWeek, endOfWeek } = require('date-fns');
 
-function getCurrentCalendarWeek() {
+function getCurrentCalendarWeek(timezone = 'UTC') {
     const today = new Date();
-    const startOfWeek = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate()));
-    startOfWeek.setUTCDate(startOfWeek.getUTCDate() - startOfWeek.getUTCDay());
+    const localNow = utcToZonedTime(now, timezone);
 
-    const endOfWeek = new Date(startOfWeek);
-    endOfWeek.setUTCDate(endOfWeek.getUTCDate() + 6);
+    const weekStart = startOfWeek(localNow, { weekStartsOn: 0 });
+    const weekEnd = endOfWeek(localNow, { weekStartsOn: 0 });
 
-    return { startOfWeek, endOfWeek };
+    const startUTC = zonedTimeToUtc(weekStart, timezone);
+    const endUTC = zonedTimeToUtc(weekEnd, timezone);
+
+    return { startOfWeek: startUTC, endOfWeek: endUTC };
 }
 
 function getGoalLogsThisWeek(goalLogs, timezone) {
     const now = new Date();
     const localNow = utcToZonedTime(now, timezone);
 
-    const weekStart = startOfWeek(localNow, { weekStartsOn: 0 }); // Sunday
-    const weekEnd = endOfWeek(localNow, { weekStartsOn: 0 }); // Saturday
+    const weekStart = startOfWeek(localNow, { weekStartsOn: 0 });
+    const weekEnd = endOfWeek(localNow, { weekStartsOn: 0 });
 
     const startUTC = zonedTimeToUtc(weekStart, timezone);
     const endUTC = zonedTimeToUtc(weekEnd, timezone);
