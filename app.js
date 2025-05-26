75825f4c75d3f7fd772871f66f380c20bf770268
Steve Hull <hullstevenj@gmail.com>
2025-05-26 12:36:56 -0700

    Add use express.json to app.js

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
 app.set('layout', 'layouts/main-layout');
 
+app.use(express.json());
+
 app.use(session({
     secret: '4d3bca5fa3e385395f69bac991078cef4eef4be66f1f816503bdc0a8359639f14a9618efa3efbd3646e5ebab9343c327516c48e195ef8397adaf70077a4de836',
     resave: false,
