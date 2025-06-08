a6a3f87caf235e494e3439b6c36c8288422c1352
Steve Hull <hullstevenj@gmail.com>
2025-06-08 09:16:08 -0700

    Comment out getGoalLogsThisWeek and add testing capable getGoalLogsThisWeek with functional override date

--- utils/goalHelpers.js
+++ utils/goalHelpers.js
@@ -13,7 +13,7 @@ function getCurrentCalendarWeek(timezone = 'UTC') {
 
     return { startOfWeek: startUTC, endOfWeek: endUTC };
 }
-
+/*
 function getGoalLogsThisWeek(goalLogs, timezone) {
     const now = utcToZonedTime(new Date(), timezone); // Always user's local time
 
@@ -25,9 +25,37 @@ function getGoalLogsThisWeek(goalLogs, timezone) {
 
     const filteredLogs = goalLogs.map(log => log.sessionDate).filter(date => date >= startDay && date <= endDay);
 
+    console.log('[📅] Week Start:', startDay);
+    console.log('[📅] Week End:', endDay);
+    console.log('[🧾] All Logs:', goalLogs.map(log => log.sessionDate));
+
+
     return filteredLogs;
 }
+*/
 
+function getGoalLogsThisWeek(goalLogs, timezone, overrideDate = null) {
+    const now = overrideDate
+        ? utcToZonedTime(new Date(overrideDate), timezone)
+        : utcToZonedTime(new Date(), timezone);
+
+    const weekStart = startOfWeek(now, { weekStartsOn: 0 });
+    const weekEnd = endOfWeek(now, { weekStartsOn: 0 });
+
+    const startDay = `${weekStart.getFullYear()}-${(weekStart.getMonth() + 1).toString().padStart(2, '0')}-${weekStart.getDate().toString().padStart(2, '0')}`;
+    const endDay = `${weekEnd.getFullYear()}-${(weekEnd.getMonth() + 1).toString().padStart(2, '0')}-${weekEnd.getDate().toString().padStart(2, '0')}`;
+
+    console.log('[📅] Week Start:', startDay);
+    console.log('[📅] Week End:', endDay);
+    console.log('[🧾] All Logs:', goalLogs.map(log => log.sessionDate));
+
+    const filteredLogs = goalLogs.filter(log => {
+        const date = log.sessionDate;
+        return date >= startDay && date <= endDay;
+    });
+
+    return filteredLogs;
+}
 
 
 
