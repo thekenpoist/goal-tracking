4fc6693f47464ebcfe625ac20fc40b74934679a2
Steve Hull <hullstevenj@gmail.com>
2025-06-10 21:22:40 -0700

    Create goalStats.js in utils for logging lastLoggedAt in the goals table

---
+++ utils/goalStats.js
@@ -0,0 +1,16 @@
+const { Goal, GoalLog } = require('../models');
+
+async function updateLastLoggedAt(goalUuid, userUuid) {
+    const latestLog = await GoalLog.findOne({
+        where: { goalUuid, userUuid },
+        order: [['sessionDate', 'DESC']]
+    });
+
+    const goal = await Goal.findOne({ where: { uuid: goalUuid, userUuid }});
+    if (!goal) return;
+
+    goal.lastLoggedAt = latestLog ? latestLog.sessionDate : null;
+    await goal.save();
+}
+
+module.exports = { updateLastLoggedAt };
