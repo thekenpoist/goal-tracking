ae7464e99bc4629e13d8de8347121c346309f67c
Steve Hull <hullstevenj@gmail.com>
2025-06-10 20:21:54 -0700

    Add res.render to viewStatsPartial in statsController

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -18,13 +18,19 @@ exports.viewStatsPartial = async (req, res, next) => {
         if (!goal) {
             return res.status(404).send('<p class="text-red-500">Goal Not Found</p>');
         }
+
+        res.render('partials/stats/stats-details', {
+            goal,
+            pageTitle: `Stats: ${goal.title}`,
+            layout: false
+        });
+
     } catch (err) {
         logger.error(`Error loading stats: ${err.message}`);
         if (err.stack) {
             logger.error(err.stack);
         }
-        
+
         res.status(500).send('<p> class="text-red-500">Server error loading stats</p>');
     }
-    
 }
