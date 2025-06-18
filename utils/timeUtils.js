9622d8c689f0781835f9c25b9c2e3e97a4a55a96
Steve Hull <hullstevenj@gmail.com>
2025-06-18 14:40:45 -0700

    Create normalizeDate in timeUtils for normalizing any date to YYYY-MM-DD

--- utils/timeUtils.js
+++ utils/timeUtils.js
@@ -1,5 +1,6 @@
 const { utcToZonedTime } = require('date-fns-tz');
 const { startOfWeek, endOfWeek, subWeeks } = require('date-fns');
+const { silly } = require('winston');
 
 // Remember, offset = 1 is one week, not one day
 function getWeekRange(offset = 0, timezone = 'UTC') {
@@ -14,7 +15,15 @@ function getWeekRange(offset = 0, timezone = 'UTC') {
 }
 
 
+function normalizeDate(dateInput) {
+    if (!dateInput) return null;
+    if (typeof dateInput === 'string') return dateInput.slice(0, 10);
+    if (dateInput instanceof Date) return dateInput.toISOString().slice(0, 10);
+    return null
+}
+
 
 module.exports = {
-    getWeekRange
+    getWeekRange,
+    normalizeDate
 };
